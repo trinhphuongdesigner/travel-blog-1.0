246a03cf240fa598626a19e2b07aca65703828b5
Trinhphuong <phuong.trinh@orientsoftware.net>
2021-04-14 10:39:47 +0700

    config routes

--- modules/routes.js
+++ modules/routes.js
@@ -1,23 +1,24 @@
-const bookmarkFolderRouter = require('./bookmarkFollder/routes');
-const bookmarkLinkRouter = require('./bookmarkLink/routes');
-const categoryRouter = require('./category/routes');
-const commentRouter = require('./comment/routes');
-const postActivityRouter = require('./postActivity/routes');
-const postsRouter = require('./posts/routes');
-const userCommentRouter = require('./userComment/routes');
-const userFollowerRouter = require('./userFollower/routes');
-const usersRouter = require('./users/routes');
+const express = require("express");
+const router = express.Router();
 
+const bookmarkFolderRouter = require("./bookmarkFollder/routes");
+const bookmarkLinkRouter = require("./bookmarkLink/routes");
+const categoryRouter = require("./category/routes");
+const commentRouter = require("./comment/routes");
+const postActivityRouter = require("./postActivity/routes");
+const postsRouter = require("./posts/routes");
+const userCommentRouter = require("./userComment/routes");
+const userFollowerRouter = require("./userFollower/routes");
+// const userRouter = require('./users/routes');
 
+router.use("/bookmark-folder", bookmarkFolderRouter);
+router.use("/bookmark-link", bookmarkLinkRouter);
+router.use("/category", categoryRouter);
+router.use("/comment", commentRouter);
+router.use("/post-activity", postActivityRouter);
+router.use("/posts", postsRouter);
+router.use("/user-comment", userCommentRouter);
+router.use("/user-follower", userFollowerRouter);
+// router.use("/user", userRouter);
 
-module.exports = {
-    bookmarkFolderRouter,
-    bookmarkLinkRouter,
-    categoryRouter,
-    commentRouter,
-    postActivityRouter,
-    postsRouter,
-    userCommentRouter,
-    userFollowerRouter,
-    usersRouter
-}
+module.exports = router;
